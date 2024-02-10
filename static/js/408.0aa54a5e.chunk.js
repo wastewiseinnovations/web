b31536d285bb27340681aabc62159388b90ee6cb
BrunoDavidES <bm.david@campus.fct.unl.pt>
2024-02-10 17:08:41 +0000

    Updates

---
+++ static/js/408.0aa54a5e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwastewiseinnovations=self.webpackChunkwastewiseinnovations||[]).push([[408],{408:(s,e,n)=>{n.r(e),n.d(e,{default:()=>t});n(2791);var a=n(7689),i=n(184);const t=()=>{const s=(0,a.TH)().state;return(0,i.jsxs)("div",{children:[(0,i.jsxs)("p",{children:["name: ",s.name]}),(0,i.jsxs)("p",{children:["email: ",s.email]}),(0,i.jsxs)("p",{children:["subject: ",s.subject]}),(0,i.jsxs)("p",{children:["message: ",s.message]})]})}}}]);
+//# sourceMappingURL=408.0aa54a5e.chunk.js.map
